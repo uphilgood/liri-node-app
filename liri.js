76f488ec11ceb63d5aada80f3b8950ef7eed9ccb
phil <philgoodmusic@gmail.com>
2018-07-17 21:32:14 -0400

    tweet and song title feature added

---
+++ liri.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+
+let keys = require("./keys")
+
+let Twitter = require('twitter');
+let Spotify = require('node-spotify-api');
+let params = {
+    screen_name: 'PhilipL47617143'
+};
+let argument = process.argv[2]
+let title = process.argv[3]
+let spotify = new Spotify(keys.spotify);
+let client = new Twitter(keys.twitter);
+
+
+if (argument === "spotify-this-song") {
+    spotify.search({
+        type: 'track',
+        query: title,
+        limit: 1
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        //gives artist name
+        let songData = data.tracks.items
+        songData.forEach(song => {
+            console.log(song.artists[0].name)
+            console.log(song.name)
+            console.log(song.artists[0].external_urls.spotify)
+            console.log(song.album.name)
+        })
+
+    });
+}
+
+if (argument === "my-tweets") {
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            tweets.forEach(tweet =>
+                console.log(tweet.text)
+            );
+        }
+    });
+}
+
+// client.post('statuses/update', {status: 'I am a tweet'}, function(error, tweet, response) {
+//     if (!error) {
+//       console.log(tweet);
+//     }
+//   });
+
+
+
+
+
+
+
