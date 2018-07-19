79ce6ecae32328754bd5b9b60dec9ee154239f44
phil <philgoodmusic@gmail.com>
2018-07-19 19:17:59 -0400

    added the randomtxt function

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 let keys = require("./keys")
-
+let fs = require("fs");
 let Twitter = require('twitter');
 let Spotify = require('node-spotify-api');
 let request = require('request');
@@ -13,38 +13,42 @@ let title = process.argv[3]
 let spotify = new Spotify(keys.spotify);
 let client = new Twitter(keys.twitter);
 let omdbKey = keys.omdb.key
+// global.navigator = {
+//     userAgent: 'liri.js'
+//   };
 
 if (argument === "movie-this" && title) {
-request.get('http://www.omdbapi.com/?t='+ title + '&apikey=' + omdbKey, function (error, response, body) {
-  if(!error) {
-      let data = JSON.parse(body)
-        console.log("Title: ", data.Title)
-        console.log("Release Date: ", data.Year)
-        console.log("IMDB Rating: ", data.imdbRating)
-        console.log("Rotten Tomatoes Rating: ", data.Ratings[1].Value)
-        console.log("Country: ", data.Country)
-        console.log("Language: ", data.Language)
-        console.log("Plot: ", data.Plot)
-        console.log("Actors: ", data.Actors)
+    request.get('http://www.omdbapi.com/?t=' + title + '&apikey=' + omdbKey, function (error, response, body) {
+        if (!error) {
+            let data = JSON.parse(body)
+            console.log("Title: ", data.Title)
+            console.log("Release Date: ", data.Year)
+            console.log("IMDB Rating: ", data.imdbRating)
+            console.log("Rotten Tomatoes Rating: ", data.Ratings[1].Value)
+            console.log("Country: ", data.Country)
+            console.log("Language: ", data.Language)
+            console.log("Plot: ", data.Plot)
+            console.log("Actors: ", data.Actors)
 
-  } else console.log(error)
-});
-} else {
+        } else console.log(error)
+    });
+}
+if (argument === "movie-this") {
     request.get('http://www.omdbapi.com/?t=Mr. Nobody&apikey=' + omdbKey, function (error, response, body) {
-  if(!error) {
-      let data = JSON.parse(body)
-        console.log("Title: ", data.Title)
-        console.log("Release Date: ", data.Year)
-        console.log("IMDB Rating: ", data.imdbRating)
-        console.log("Rotten Tomatoes Rating: ", data.Ratings[1].Value)
-        console.log("Country: ", data.Country)
-        console.log("Language: ", data.Language)
-        console.log("Plot: ", data.Plot)
-        console.log("Actors: ", data.Actors)
+        if (!error) {
+            let data = JSON.parse(body)
+            console.log("Title: ", data.Title)
+            console.log("Release Date: ", data.Year)
+            console.log("IMDB Rating: ", data.imdbRating)
+            console.log("Rotten Tomatoes Rating: ", data.Ratings[1].Value)
+            console.log("Country: ", data.Country)
+            console.log("Language: ", data.Language)
+            console.log("Plot: ", data.Plot)
+            console.log("Actors: ", data.Actors)
 
-  } else console.log(error)
-});
-    
+        } else console.log(error)
+    });
+
 }
 
 
@@ -79,15 +83,46 @@ if (argument === "my-tweets") {
 }
 
 if (argument === "tweet")
-client.post('statuses/update', {status: title}, function(error, tweet, response) {
-    if (!error) {
-      console.log(tweet);
+    client.post('statuses/update', {
+        status: title
+    }, function (error, tweet, response) {
+        if (!error) {
+            console.log(tweet);
+        }
+    });
+
+if (argument === "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+          return console.log(error);
+        }
+      
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+
+        spotify.search({
+            type: 'track',
+            query: dataArr[1],
+            limit: 1
+        }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            //gives artist name
+            let songData = data.tracks.items
+            songData.forEach(song => {
+                console.log(song.artists[0].name)
+                console.log(song.name)
+                console.log(song.artists[0].external_urls.spotify)
+                console.log(song.album.name)
+            })
+        });
+      
+      
+      });
     }
-  });
+   
 
-
-
-
-
-
-
+ 
