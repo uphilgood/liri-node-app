8a6267ee194d56ae6f183203e2767875d273086c
phil <philgoodmusic@gmail.com>
2018-07-19 20:53:50 -0400

    finished project

--- liri.js
+++ liri.js
@@ -13,45 +13,39 @@ let title = process.argv[3]
 let spotify = new Spotify(keys.spotify);
 let client = new Twitter(keys.twitter);
 let omdbKey = keys.omdb.key
-// global.navigator = {
-//     userAgent: 'liri.js'
-//   };
 
-if (argument === "movie-this" && title) {
-    request.get('http://www.omdbapi.com/?t=' + title + '&apikey=' + omdbKey, function (error, response, body) {
+omdb = (search, key) => {
+    request.get('http://www.omdbapi.com/?t=' + search + '&apikey=' + key, function (error, response, body) {
         if (!error) {
             let data = JSON.parse(body)
-            console.log("Title: ", data.Title)
-            console.log("Release Date: ", data.Year)
-            console.log("IMDB Rating: ", data.imdbRating)
-            console.log("Rotten Tomatoes Rating: ", data.Ratings[1].Value)
-            console.log("Country: ", data.Country)
-            console.log("Language: ", data.Language)
-            console.log("Plot: ", data.Plot)
-            console.log("Actors: ", data.Actors)
-
+            console.log("Title: " + data.Title + "\nRelease Date:: " + data.Year+
+            "\nIMDB Rating: " + data.imdbRating + "\nRotten Tomatoes Rating: " + data.Ratings[1].Value + "\nCountry: " + data.Country 
+            + "\nLanguage: " + data.Language + "\nPlot: " + data.Plot + "\nActors: " + data.Actors);
+            append(data.Title)
+            append(data.Year)
+            append(data.imdbRating)
+            append(data.Ratings[1].Value )
+            append(data.Country)
+            append(data.Language)
+            append(data.Plot)
+            append(data.Actors)
         } else console.log(error)
     });
 }
-if (argument === "movie-this") {
-    request.get('http://www.omdbapi.com/?t=Mr. Nobody&apikey=' + omdbKey, function (error, response, body) {
+
+getTweets = params => {
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
-            let data = JSON.parse(body)
-            console.log("Title: ", data.Title)
-            console.log("Release Date: ", data.Year)
-            console.log("IMDB Rating: ", data.imdbRating)
-            console.log("Rotten Tomatoes Rating: ", data.Ratings[1].Value)
-            console.log("Country: ", data.Country)
-            console.log("Language: ", data.Language)
-            console.log("Plot: ", data.Plot)
-            console.log("Actors: ", data.Actors)
-
-        } else console.log(error)
+            tweets.forEach(tweet => {
+                console.log(tweet.text + tweet.created_at)
+                append(tweet.text + tweet.created_at)
+            }
+            );
+        }
     });
-
 }
 
-if (argument === "spotify-this-song") {
+searchTrack = title => {
     spotify.search({
         type: 'track',
         query: title,
@@ -63,65 +57,66 @@ if (argument === "spotify-this-song") {
         //gives artist name
         let songData = data.tracks.items
         songData.forEach(song => {
-            console.log(song.artists[0].name)
-            console.log(song.name)
-            console.log(song.artists[0].external_urls.spotify)
-            console.log(song.album.name)
+            console.log("Name: " + song.artists[0].name + "\nSong Name: " + song.name +
+            "\nSong URL: " + song.artists[0].external_urls.spotify + "\nAlbum Name: " + song.album.name);
+            append(song.artists[0].name)
+            append(song.name)
+            append(song.artists[0].external_urls.spotify)
+            append(song.album.name)
         })
     });
 }
 
-if (argument === "my-tweets") {
-    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+postTweet = message => {
+    client.post('statuses/update', {
+        status: message
+    }, function (error, tweet, response) {
         if (!error) {
-            tweets.forEach(tweet =>
-                console.log(tweet.text)
-            );
+            append(tweet.text);
         }
     });
 }
 
-if (argument === "tweet")
-    client.post('statuses/update', {
-        status: title
-    }, function (error, tweet, response) {
-        if (!error) {
-            console.log(tweet);
-        }
-    });
-
-if (argument === "do-what-it-says") {
-    fs.readFile("random.txt", "utf8", function(error, data) {
-
+randomFile = () => {
+    fs.readFile("random.txt", "utf8", function (error, data) {
         // If the code experiences any errors it will log the error to the console.
         if (error) {
-          return console.log(error);
+            return console.log(error);
         }
-      
         // Then split it by commas (to make it more readable)
-        var dataArr = data.split(",");
+        let dataArr = data.split(",");
+       searchTrack(dataArr[1])
+    });
+}
 
-        spotify.search({
-            type: 'track',
-            query: dataArr[1],
-            limit: 1
-        }, function (err, data) {
-            if (err) {
-                return console.log('Error occurred: ' + err);
-            }
-            //gives artist name
-            let songData = data.tracks.items
-            songData.forEach(song => {
-                console.log(song.artists[0].name)
-                console.log(song.name)
-                console.log(song.artists[0].external_urls.spotify)
-                console.log(song.album.name)
-            })
-        });
-      
-      
-      });
-    }
-   
+append = (arg) => {
+    fs.appendFile("log.txt", "\n" + arg, function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
 
- 
+if (argument === "movie-this" && title) {
+    omdb(title, omdbKey)
+} else if (argument === "movie-this") {
+    omdb("Mr. Nobody", omdbKey)
+
+}
+
+if (argument === "spotify-this-song") {
+    searchTrack(title);
+} 
+
+if (argument === "my-tweets") {
+    getTweets(params);
+}
+
+if (argument === "tweet") {
+    postTweet(title);
+}
+
+if (argument === "do-what-it-says") {
+    randomFile();
+}
+
